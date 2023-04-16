59ac39d8e30a7e91b1f76b26039bc1cdf8d37c0d
Lily LaCosse <lilylacosse@gmail.com>
2023-04-16 12:36:39 -0500

    added /api/plants route to server

--- server/routes/plants.router.js
+++ server/routes/plants.router.js
@@ -1,18 +1,16 @@
-const express = require('express');
-const pool = require('../modules/pool');
+const express = require("express");
+const pool = require("../modules/pool");
 const router = express.Router();
 
 /**
  * GET route template
  */
-router.get('/', (req, res) => {
-  // GET route code here
-});
+router.get("/", (req, res) => {});
 
 /**
  * POST route template
  */
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   // POST route code here
 });
 
