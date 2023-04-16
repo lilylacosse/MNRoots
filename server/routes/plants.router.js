a832d696d42d44fdd11ee05003f934878b7b964c
Lily LaCosse <lilylacosse@gmail.com>
2023-04-16 18:44:57 -0500

    api connect fixed via correct string interpolation and file call for API key

--- server/routes/plants.router.js
+++ server/routes/plants.router.js
@@ -2,28 +2,20 @@ const axios = require("axios");
 const express = require("express");
 const pool = require("../modules/pool");
 const router = express.Router();
-const fetch = import("node-fetch");
-//  https GET request to Trefle API to retrieve plant list to server to user based on user input
-// router.get("/", (req, res) => {
-//   axios
-//     .get("https://trefle.io/api/v1/plants?token=TREFLE_API_KEY")
-//     .then((response) => {
-//       console.log(`trefle data:`, response.data);
-//       return res.send(response.data);
-//     })
-//     .catch((err) => {
-//       console.log(`get TREFLE PLANT LIST error:`, err);
-//       res.sendStatus(500);
-//     });
-// });
 
-async () => {
-  const response = await fetch(
-    "https://trefle.io/api/v1/plants?token=TREFLE_API_KEY"
-  );
-  const json = await response.json();
-  console.log(json);
-};
+//  https GET request to Trefle API to retrieve plant list to server to user based on user input
+router.get("/", (req, res) => {
+  axios
+    .get(`https://trefle.io/api/v1/plants?token=${process.env.TREFLE_API_KEY}`)
+    .then((response) => {
+      console.log(`trefle data:`, response.data);
+      return res.send(response.data);
+    })
+    .catch((err) => {
+      console.log(`get TREFLE PLANT LIST error:`, err);
+      res.sendStatus(500);
+    });
+});
 
 /**
  * POST route template
