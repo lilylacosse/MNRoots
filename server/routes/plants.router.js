5e1b40bfdc15c489077d352cc85bb66b942d887f
Lily LaCosse <lilylacosse@gmail.com>
2023-04-16 18:28:21 -0500

    installed node-fetch, seems to have helped with https request aspect

--- server/routes/plants.router.js
+++ server/routes/plants.router.js
@@ -1,11 +1,29 @@
+const axios = require("axios");
 const express = require("express");
 const pool = require("../modules/pool");
 const router = express.Router();
+const fetch = import("node-fetch");
+//  https GET request to Trefle API to retrieve plant list to server to user based on user input
+// router.get("/", (req, res) => {
+//   axios
+//     .get("https://trefle.io/api/v1/plants?token=TREFLE_API_KEY")
+//     .then((response) => {
+//       console.log(`trefle data:`, response.data);
+//       return res.send(response.data);
+//     })
+//     .catch((err) => {
+//       console.log(`get TREFLE PLANT LIST error:`, err);
+//       res.sendStatus(500);
+//     });
+// });
 
-/**
- * GET route template
- */
-router.get("/", (req, res) => {});
+async () => {
+  const response = await fetch(
+    "https://trefle.io/api/v1/plants?token=TREFLE_API_KEY"
+  );
+  const json = await response.json();
+  console.log(json);
+};
 
 /**
  * POST route template
