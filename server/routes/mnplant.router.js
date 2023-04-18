bec2fbee187c68a2b5c3d63bba328a5f43b5e15f
Lily LaCosse <lilylacosse@gmail.com>
2023-04-18 14:14:43 -0500

    created and updated favPlant router with CRUD routes

--- server/routes/mnplant.router.js
+++ server/routes/mnplant.router.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const pool = require("../modules/pool");
 const router = express.Router();
+const {
+  rejectUnauthenticated,
+} = require("../modules/authentication-middleware");
 
 /**
  * GET route template
  */
-router.get("/", (req, res) => {
+router.get("/", rejectUnauthenticated, (req, res) => {
   const sqlText = `select * from native_plants`;
   pool
     .query(sqlText)
