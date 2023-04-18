d77da4b55dc01b96ce49f540c868222a3b931692
Lily LaCosse <lilylacosse@gmail.com>
2023-04-18 09:54:37 -0500

    getting ahead of myself - beginning to think through put to users_favorites table in db, but need to have render sorted first

--- src/components/MnPlants/MnPlants.jsx
+++ src/components/MnPlants/MnPlants.jsx
@@ -2,9 +2,11 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 function MnPlants(props) {
     const dispatch = useDispatch();
+    const history = useHistory();
     const mnPlants = useSelector((store) => store.mnPlants);
     console.log(`mnPlants in MnPlants Component:`, mnPlants);
     const [heading, setHeading] = useState("Functional Component");
@@ -14,24 +16,33 @@ function MnPlants(props) {
         dispatch({ type: "FETCH_MN_PLANTS" });
     }, []);
 
-    return (
-        <div>
-            <h2>Minnesota Native Plants</h2>
-            <div>
-                {mnPlants.map((plant) => {
-                    return (
-                        <div key={plant.id}>
-                            <div>{plant.common_name}</div>
-                            <button>Save to Your Garden</button>
+    function dispatchPlant(plant) {
+        dispatch({
+            type: "",
+            payload: plant
+        })
+    }
+}
 
-                            {/* <div>{plant.images.leaf.map((pic) => { return (<div key={pic.id}><img src={pic.image_url} /></div>) })} */}
-                            {/* I THINK THIS IMAGE ASPECT NEEDS TO BE ITS OWN COMPONENT */}
-                        </div>
-                    );
-                })}
-            </div>
+return (
+    <div>
+        <h2>Minnesota Native Plants</h2>
+        <button onClick={() => { history.push('/yourgarden') }}>Go to Your Garden</button>
+        <div>
+            {mnPlants.map((plant) => {
+                return (
+                    <div key={plant.id}>
+                        <div>{plant.common_name}</div>
+                        <button onClick={() => dispatchPlant(plant)} >Save to Your Garden</button>
+
+                        {/* <div>{plant.images.leaf.map((pic) => { return (<div key={pic.id}><img src={pic.image_url} /></div>) })} */}
+                        {/* I THINK THIS IMAGE ASPECT NEEDS TO BE ITS OWN COMPONENT */}
+                    </div>
+                );
+            })}
         </div>
-    );
+    </div>
+);
 }
 
 export default MnPlants;
