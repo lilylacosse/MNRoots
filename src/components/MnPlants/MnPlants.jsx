c158c976fd021aa15a9845dbd4a5d36f0089c23a
Lily LaCosse <lilylacosse@gmail.com>
2023-04-18 09:23:38 -0500

    moving on to post route

--- src/components/MnPlants/MnPlants.jsx
+++ src/components/MnPlants/MnPlants.jsx
@@ -1,14 +1,13 @@
-import React, { useState } from 'react';
-import { useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
-
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 function MnPlants(props) {
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
     const mnPlants = useSelector((store) => store.mnPlants);
     console.log(`mnPlants in MnPlants Component:`, mnPlants);
-    const [heading, setHeading] = useState('Functional Component');
+    const [heading, setHeading] = useState("Functional Component");
     // console.log(`plant.images.leaf:`, mnPlants[6].images.leaf[0]);
 
     useEffect(() => {
@@ -18,14 +17,18 @@ function MnPlants(props) {
     return (
         <div>
             <h2>Minnesota Native Plants</h2>
-            <div>{mnPlants.map((plant) => {
-                return (<div key={plant.id}>
-                    <div>{plant.common_name}</div>
+            <div>
+                {mnPlants.map((plant) => {
+                    return (
+                        <div key={plant.id}>
+                            <div>{plant.common_name}</div>
+                            <button>Save to Your Garden</button>
 
-                    <div>{plant.images.leaf.map((pic) => { return (<div key={pic.id}><img src={pic.image_url} /></div>) })}</div>
-                </div>)
-            })}</div>
-
+                            {/* <div>{plant.images.leaf.map((pic) => { return (<div key={pic.id}><img src={pic.image_url} /></div>) })} */}
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 }
