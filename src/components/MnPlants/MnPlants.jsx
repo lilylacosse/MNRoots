8f352722b6f9b513883fb800fe1648f2413a48a3
Lily LaCosse <lilylacosse@gmail.com>
2023-04-20 16:33:22 -0500

    laid line/base logic for conditional rendering (ie color change) to indicate that a plant has been added to your garden

--- src/components/MnPlants/MnPlants.jsx
+++ src/components/MnPlants/MnPlants.jsx
@@ -13,33 +13,47 @@ function MnPlants() {
     const dispatch = useDispatch();
     const history = useHistory();
     const mnPlants = useSelector((store) => store.mnPlants);
-    console.log(`mnPlants in MnPlants Component:`, mnPlants);
 
-    const [favPlant, setFavPlant] = useState("");
-    // console.log(`plant.images.leaf:`, mnPlants[6].images.leaf[0]);
 
     useEffect(() => {
         dispatch({ type: "FETCH_MN_PLANTS" });
     }, []);
+
+    const selectedPlants = [];
+    // if plant id is in selectedPlants array, conditionally render that card a new color 
+
+    function updateSelectedPlants(newFavPlant) {
+        console.log(`selectedPlants OG:`, selectedPlants);
+        selectedPlants.push(newFavPlant);
+        console.log(`selectedPlants Now:`, selectedPlants);
+    }
 
     function dispatchPlant(plantId) {
         dispatch({
             type: "ADD_MY_PLANT",
             payload: plantId
         })
+        updateSelectedPlants(plantId)
     }
 
     return (
         <center>
             <h2>Minnesota Native Plants</h2>
+            <div>Peruse through Minnesota's wealth of native plants, save plants you like the most to plant in your native garden</div>
+            <br />
+            <div>Plants and Plant Data are sourced from the Minnesota Bell Museum</div>
+            <br />
             <button onClick={() => { history.push('/mygarden') }}>View My Garden</button>
+
+            <br />
             {/* TO DO - Add CARD features */}
             <div>{mnPlants ?
 
                 mnPlants.map((plant) => {
                     return (
                         <div key={plant.id}>
-                            <img src={plant.image_url} />
+                            <img src={plant.image_url} width="200" height='280' />
+                            <br />
                             <button onClick={() => dispatchPlant(plant.id)} >Save to My Garden</button>
                         </div>
                     );
